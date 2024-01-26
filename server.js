eae1f6641f692d1470b598cf10b23dd16f0c7fc9
Lance Tse <steam.bao666@gmail.com>
2024-01-26 12:30:26 -0800

    add get route to /bakery

--- server.js
+++ server.js
@@ -10,18 +10,24 @@ app.use(express.json());
 const client = new pg.Client({
     // TODO: Replace "postgres://localhost/example_db" with process.env.DATABASE_URL
     connectionString: process.env.DATABASE_URL
-  });
+});
   
-  await client.connect();
-  
-  app.use(express.static("public"));
-  
-  app.get("/api/students", (_, res) => {
-    client.query("SELECT * FROM student").then((data) => {
-      res.json(data.rows);
-    });
-  });
-  
-  app.listen(process.env.PORT, () => {
-    console.log(`Server listening on port: ${process.env.PORT}.`);
-  });
+await client.connect();
+
+app.use(express.static("public"));
+
+app.get("/bakery", (req, res) => {
+  console.log(`get request to route /bakery`);
+  client.query("SELECT * FROM bakery")
+  .then((data) => {
+    res.json(data.rows);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port: ${process.env.PORT}.`);
+});
