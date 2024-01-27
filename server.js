a4b7cf20bbda951706e32116a29639b694096c50
Lance Tse <steam.bao666@gmail.com>
2024-01-26 17:18:57 -0800

    add RETURNING * to end of post query

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post("/person/:name", (req, res) => {
 
   console.log(`Request to post typeof name: ${typeof req.params.name}, name: ${name}, money: ${money}`);
 
-  client.query(`INSERT INTO person (person_name, person_money) VALUES ($1, $2)`, [name, money])
+  client.query(`INSERT INTO person (person_name, person_money) VALUES ($1, $2) RETURNING *`, [name, money])
   .then((data) => {
     console.log(data.rows[0]);
     res.json(data.rows[0]);
