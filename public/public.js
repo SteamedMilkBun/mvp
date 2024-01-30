5b79f97ad6951cb8e77f075f387acd971dc9a63a
Lance Tse <steam.bao666@gmail.com>
2024-01-30 11:08:06 -0800

    begin client side

---
+++ public/public.js
@@ -0,0 +1,14 @@
+//show all people in person table
+let showPersonTable = function () {
+    //create personDiv container to append later
+    const $personDiv = $('<div></div>');
+    $personDiv.append('<p>p in personDiv</p>');
+    //append #personDiv to people container
+    $("body").append($personDiv);
+}
+//start by selecting personDiv class to manipulate the div
+
+
+//show all baked goods in baked_goods table
+
+showPersonTable();
