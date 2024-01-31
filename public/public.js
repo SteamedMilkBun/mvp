f0e47b22ca179cef354d17fdd052a801b4d1fad9
Lance Tse <steam.bao666@gmail.com>
2024-01-30 18:13:51 -0800

    move remove containers back into show functions

--- public/public.js
+++ public/public.js
@@ -5,7 +5,7 @@ const createPeopleContainer = function () {
     const $clickable = $('<div>Show All People</div>');
     $clickable.addClass("clickable");
     $clickable.on('click', () => {
-        $(".people-container").remove();
+        
         fetch("person")
         .then((fetchResult) => {
             return fetchResult.json();
@@ -25,7 +25,7 @@ const createPeopleContainer = function () {
 
 const createBakedGoodsContainer = function () {
     const $bakedGoodsContainer = $('<div></div>')
-    $(".bg-container").addClass("bg-container");
+    
 
     const $clickable = $('<div>Show All Baked Goods</div>')
     $clickable.addClass("clickable");
@@ -50,7 +50,7 @@ const createBakedGoodsContainer = function () {
 
 //show all people in person table
 const showPeople = function (people) {
-
+    $(".people-container").remove();
     for (let person of people) {
         //create personDiv container to append later
         const $personDiv = $(`<div>${person.person_name}</div>`);
@@ -62,7 +62,7 @@ const showPeople = function (people) {
 
 //show all baked goods in baked_goods table
 const showBakedGoods = function (baked_goods) {
-
+    $(".bg-container").addClass("bg-container");
     for (let baked_good of baked_goods) {
         const $bgDiv = $(`<div>${baked_good.baked_goods_name}: ${baked_good.baked_goods_price} copper</div>`);
         $bgDiv.addClass("container");
