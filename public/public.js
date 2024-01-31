da9bbe8c9bd5e35e735c1ff8a4432e76972044c6
Lance Tse <steam.bao666@gmail.com>
2024-01-30 16:32:04 -0800

    flesh out showBakedGoods

--- public/public.js
+++ public/public.js
@@ -29,13 +29,23 @@ const createBakedGoodsContainer = function () {
     const $bakedGoodsContainer = $('<div>This is the bakedGoodsContainer</div>')
     $bakedGoodsContainer.addClass("container");
 
-    const $clickable = $('<div>Show All</div>')
+    const $clickable = $('<div>Show All Baked Goods</div>')
     $clickable.addClass("clickable");
     $clickable.on('click', () => {
-        console.log("clicked the div");
-        let resultArr = getTable();
-        console.log(resultArr);
-        return resultArr;
+        fetch("baked_goods")
+        .then((fetchResult) => {
+            //turn fetchResult from string into json
+            return fetchResult.json();
+        })
+        .then((jsonData) => {
+            console.log(`pass jsonData into showBakedGoods`);
+            showBakedGoods(jsonData);
+            console.log("f: showBakedGoods finished")
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500); 
+        })
     });
 
     $("body").append($bakedGoodsContainer);
@@ -57,10 +67,15 @@ const showPeople = function (people) {
 }
 
 //show all baked goods in baked_goods table
-const showBakedGoods = function () {
-    const $bgDiv = $('<div></div>').addClass("container");
-    $bgDiv.append('<p>iterate to show each baked good</p>');
-    $("body").append($bgDiv);
+const showBakedGoods = function (baked_goods) {
+    console.log(`inside f:showbg`)
+    console.log(baked_goods);
+
+    for (let baked_good of bake_goods) {
+        const $bgDiv = $(`<div>${baked_good.baked_goods_name}: ${baked_good.baked_goods_price} copper</div>`);
+        $bgDiv.addClass("container");
+        $("body").append($bgDiv);
+    }
 }
 
 createPeopleContainer();
